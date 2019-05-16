7f0a774f88165da9f2bad904c74d1f85584bfd18
Young5416 <1101744096@qq.com>
2019-05-16 23:15:56 +0800

    feat(VueRouter ): VueRouter
    VueRouter路由引入及配置 页面模块引入

--- src/main.js
+++ src/main.js
@@ -1,9 +1,31 @@
 import Vue from 'vue'
 import './cube-ui'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import goods from './components/goods/goods'
+import ratings from './components/ratings/ratings'
+import seller from './components/seller/seller'
 
-Vue.config.productionTip = false
+Vue.use(VueRouter)
 
-new Vue({
+const routes = [
+  { path: '/goods', component: goods },
+  { path: '/ratings', component: ratings },
+  { path: '/seller', component: seller }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  linkActiveClass: 'active',
+  routes
+})
+
+var vm = new Vue({
+  el: '#app',
+  router,
   render: h => h(App)
-}).$mount('#app')
+})
+
+Vue.use({
+  vm
+})
