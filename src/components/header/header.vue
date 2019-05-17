f475362b0773067641588923be35389675a94c89
Young5416 <1101744096@qq.com>
2019-05-17 22:22:30 +0800

    feat(header 弹出层): header 弹出层
    header 弹出层 添加关闭按钮

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,7 +17,7 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
@@ -29,6 +29,16 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%">
     </div>
+    <div v-show="detailShow" class="detail">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close"></i>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -38,6 +48,16 @@
     props: {
       seller: {
         type: Object
+      }
+    },
+    data () {
+      return {
+        detailShow: false
+      }
+    },
+    methods: {
+      showDetail () {
+        this.detailShow = true
       }
     },
     created () {
@@ -51,6 +71,7 @@
 
   .header
     position: relative
+    overflow: hidden
     color: #fff
     background: rgba(7, 17, 27, 0.5)
 
@@ -149,7 +170,7 @@
       .bulletin-title
         display: inline-block
         vertical-align: top
-        margin-top: 7px
+        margin-top: 8px
         width: 22px
         height: 12px
         bg-image('bulletin')
@@ -175,4 +196,35 @@
       height: 100%
       z-index: -1
       filter: blur(10px)
+
+    .detail
+      position: fixed
+      z-index: 100
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      overflow: auto
+      background: rgba(7, 17, 27, 0.8)
+
+      .detail-wrapper
+        width: 100%
+        min-height: 100%
+
+        .detail-main
+          margin-top: 64px
+          padding-bottom: 64px
+          .name
+            line-height: 16px
+            text-align: center
+            font-size: 16px
+            font-weight: 700
+
+      .detail-close
+        position: relative
+        width: 32px
+        height: 32px
+        margin: -64px auto 0 auto
+        clear: both
+        font-size: 32px
 </style>
